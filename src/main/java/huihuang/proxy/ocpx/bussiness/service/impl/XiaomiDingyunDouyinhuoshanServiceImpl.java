0f9bd60b2790294360b7f71eb9126f4e59de0546
xietao <xietao@cdvcloud.com>
2024-01-05 08:58:01 +0800

    小米-抖音火山（丁耘）问题优化

--- src/main/java/huihuang/proxy/ocpx/bussiness/service/impl/XiaomiDingyunDouyinhuoshanServiceImpl.java
+++ src/main/java/huihuang/proxy/ocpx/bussiness/service/impl/XiaomiDingyunDouyinhuoshanServiceImpl.java
@@ -3,7 +3,7 @@ package huihuang.proxy.ocpx.bussiness.service.impl;
 import huihuang.proxy.ocpx.ads.dingyun.DingyunAdsDTO;
 import huihuang.proxy.ocpx.ads.dingyun.DingyunEventTypeEnum;
 import huihuang.proxy.ocpx.ads.dingyun.huoshan.DingyunDouyinHuoshanPath;
-import huihuang.proxy.ocpx.bussiness.dao.ads.IDingyunXiguaVideoAdsDao;
+import huihuang.proxy.ocpx.bussiness.dao.ads.IDingyunDouyinhuoshanAdsDao;
 import huihuang.proxy.ocpx.bussiness.service.BaseServiceInner;
 import huihuang.proxy.ocpx.bussiness.service.IChannelAdsService;
 import huihuang.proxy.ocpx.bussiness.service.basechannel.XiaomiChannelFactory;
@@ -29,7 +29,7 @@ public class XiaomiDingyunDouyinhuoshanServiceImpl extends XiaomiChannelFactory 
     @Autowired
     private ChannelAdsFactory channelAdsFactory;
     @Autowired
-    private IDingyunXiguaVideoAdsDao dyxgAdsDao;
+    private IDingyunDouyinhuoshanAdsDao dydyhsAdsDao;
     @Autowired
     private BaseServiceInner baseServiceInner;
     @Autowired
@@ -48,7 +48,7 @@ public class XiaomiDingyunDouyinhuoshanServiceImpl extends XiaomiChannelFactory 
         String eventType = parameterMap.get("actionType")[0];
         logger.info("adsCallBack {} 开始回调渠道  id:{}  event:{}", channelAdsKey, id, eventType);
         //根据id查询对应的点击记录
-        DingyunAdsDTO dyAdsDTO = dyxgAdsDao.queryDingyunXiguavideoAdsById(id);
+        DingyunAdsDTO dyAdsDTO = dydyhsAdsDao.queryDingyunDouyinhuoshanAdsById(id);
         if (null == dyAdsDTO) {
             logger.error("{} 未根据{}找到对应的监测信息", channelAdsKey, id);
             return BasicResult.getFailResponse("未找到对应的监测信息 " + id);
@@ -77,12 +77,12 @@ public class XiaomiDingyunDouyinhuoshanServiceImpl extends XiaomiChannelFactory 
 
         if (response.getCode() == 0) {
             dyAds.setCallBackStatus(Constants.CallBackStatus.SUCCESS.getCode());
-            baseServiceInner.updateAdsObject(dyAds, dyxgAdsDao);
+            baseServiceInner.updateAdsObject(dyAds, dydyhsAdsDao);
             logger.info("adsCallBack {} 回调渠道成功：{}", channelAdsKey, data);
             return BasicResult.getSuccessResponse(data.getId());
         } else {
             dyAds.setCallBackStatus(Constants.CallBackStatus.FAIL.getCode());
-            baseServiceInner.updateAdsObject(dyAds, dyxgAdsDao);
+            baseServiceInner.updateAdsObject(dyAds, dydyhsAdsDao);
             logger.info("adsCallBack {} 回调渠道失败：{}", channelAdsKey, data);
             return BasicResult.getFailResponse(data.getCallBackMes());
         }
