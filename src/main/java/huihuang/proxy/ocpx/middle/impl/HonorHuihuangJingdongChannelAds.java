16883e5eab6ec089e35b5f30ceeb24020defdab3
xietao <xietao@cdvcloud.com>
2024-03-26 17:12:03 +0800

    荣耀-京东上报参数调整

--- src/main/java/huihuang/proxy/ocpx/middle/impl/HonorHuihuangJingdongChannelAds.java
+++ src/main/java/huihuang/proxy/ocpx/middle/impl/HonorHuihuangJingdongChannelAds.java
@@ -1,12 +1,12 @@
 package huihuang.proxy.ocpx.middle.impl;
 
+import cn.hutool.crypto.digest.MD5;
 import huihuang.proxy.ocpx.ads.huihuangmingtian.HuihuangmingtianParamField;
 import huihuang.proxy.ocpx.bussiness.dao.ads.IHuihuangJingdongAdsDao;
 import huihuang.proxy.ocpx.channel.honor.HonorPath;
 import huihuang.proxy.ocpx.common.Constants;
 import huihuang.proxy.ocpx.marketinterface.IMarkDao;
 import huihuang.proxy.ocpx.middle.baseadsreport.huihuangmingtian.HonorHuihuangReportFactory;
-import huihuang.proxy.ocpx.middle.baseadsreport.huihuangmingtian.HuaweiHuihuangReportFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -41,14 +41,16 @@ public class HonorHuihuangJingdongChannelAds extends HonorHuihuangReportFactory 
     }
 
 
-//    @Override
-//    protected Object channelParamToAdsParam(Map<String, String[]> parameterMap) {
-//        HuihuangmingtianParamField huihuangmingtianParamField = (HuihuangmingtianParamField) super.channelParamToAdsParam(parameterMap);
-//        huihuangmingtianParamField.setApp("3");
-//        huihuangmingtianParamField.setOs("0");
-//        huihuangmingtianParamField.setEventType("1");
-//        return huihuangmingtianParamField;
-//    }
+    @Override
+    protected Object channelParamToAdsParam(Map<String, String[]> parameterMap) {
+        HuihuangmingtianParamField huihuangmingtianParamField = (HuihuangmingtianParamField) super.channelParamToAdsParam(parameterMap);
+        //辉煌京东v1.2版本，上报参数只有oaidMd5，没有oaid
+
+        if (null != huihuangmingtianParamField.getOaid() && !"".equals(huihuangmingtianParamField.getOaid()) && !huihuangmingtianParamField.getOaid().equals("__OAID__")) {
+            huihuangmingtianParamField.setOaidMd5(MD5.create().digestHex(huihuangmingtianParamField.getOaid()));
+        }
+        return huihuangmingtianParamField;
+    }
 
 
 }
