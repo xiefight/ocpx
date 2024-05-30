886e76e7f8b8d78af20ba888abb77fe7aa5efb58
xietao <xietao@yunshicloud.com>
2024-05-30 16:59:38 +0800

    广点通-闲鱼（huihui）添加新户5

--- src/main/java/huihuang/proxy/ocpx/channel/guangdiantong/GuangdiantongPath.java
+++ src/main/java/huihuang/proxy/ocpx/channel/guangdiantong/GuangdiantongPath.java
@@ -13,6 +13,7 @@ public class GuangdiantongPath {
     public static final String GUANGDIANTONG_HUIHUI_XIANYU_02 = "gdthhxy02";
     public static final String GUANGDIANTONG_HUIHUI_XIANYU_03 = "gdthhxy03";
     public static final String GUANGDIANTONG_HUIHUI_XIANYU_04 = "gdthhxy04";
+    public static final String GUANGDIANTONG_HUIHUI_XIANYU_05 = "gdthhxy05";
 
 
 }
