41027ca3691916ab22b759a8fe508c1ef1db1e18
xietao <xietao@yunshicloud.com>
2024-12-06 14:17:28 +0800

    几月家的广告主（待完善）

---
+++ src/main/java/huihuang/proxy/ocpx/ads/jiyue/JiyuePath.java
@@ -0,0 +1,12 @@
+package huihuang.proxy.ocpx.ads.jiyue;
+
+public abstract class JiyuePath {
+
+    protected abstract String baseAdsName();
+
+    /**
+     * 点击上报及转化数据回调接口
+     */
+    public static final String BASIC_URI = "http://www.wyjianzhan.com/ex/ep/x/fzk?token=7pPnE9FcwLoIp3ir24WpYq8JKJgODASzANndcy0vZ0fzSC6u2AyKbXnZH9mZw86BATAn2iUPIia3Q9QF47mrtLjYjhCbDv7Svh%2FUtfz3Nps%3D";
+
+}
